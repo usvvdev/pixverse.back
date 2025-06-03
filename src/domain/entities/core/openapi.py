4b47d0658d54107c03afcb4ef2b7a26a31a8f744
usvvdev <usv.dev@yandex.ru>
2025-06-03 14:45:25 +0300

    fix swagger

--- src/domain/entities/core/openapi.py
+++ src/domain/entities/core/openapi.py
@@ -51,7 +51,7 @@ class IOpenAPI(ISchema):
 
     openapi_prefix: Annotated[
         str,
-        Field(default=f"{getenv('APP_SERVICE')}"),
+        Field(default=f"/{getenv('APP_SERVICE')}"),
     ]
     """Префикс для всех OpenAPI-эндпоинтов.
     
