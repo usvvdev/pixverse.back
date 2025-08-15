7f3ceb800928ad700cea56aebceef4d4508a0bfb
usvvdev <usv.dev@yandex.ru>
2025-08-15 14:10:27 +0300

    added ngix

--- scripts/start_api.sh
+++ scripts/start_api.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-load_config() {
-    local env_file=".env"
+ENV_FILE=".env"
 
-    if [ ! -f "$env_file" ]; then
-        echo "ERROR" "Configuration file '$env_file' not found."
-        return 1
-    fi
+# Загружаем конфигурацию
+if [ ! -f "$ENV_FILE" ]; then
+    echo "ERROR: Configuration file '$ENV_FILE' not found."
+    exit 1
+fi
 
-    if export $(grep -v '^#' "$env_file" | xargs) > /dev/null 2>&1; then
-        echo "INFO" "Application configuration has been loaded successfully."
-    fi
-}
+export $(grep -v '^#' "$ENV_FILE" | xargs)
 
-# Функция для запуска приложения
-start_fastapi_app() {
-    echo "INFO" "Running Application."
-    uvicorn "$@"
-}
+# Проверка переменных
+: "${APP_SERVICE:?APP_SERVICE is not set in .env}"
+: "${APP_HOST:?APP_HOST is not set in .env}"
+: "${APP_PORT:?APP_PORT is not set in .env}"
 
-load_config
+echo "INFO: Running FastAPI service '$APP_SERVICE' on ${APP_HOST}:${APP_PORT}"
 
-start_fastapi_app services.${APP_SERVICE}.main:main --reload --factory --host ${APP_HOST} --port ${APP_PORT}
-# start_fastapi_app services.qwen.main:main --reload --factory --host ${APP_HOST} --port ${APP_PORT}
+# Запуск FastAPI
+uvicorn "services.${APP_SERVICE}.main:main" --reload --factory --host "${APP_HOST}" --port "${APP_PORT}"
